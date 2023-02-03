b51a23e626c07fc33d1c030aa1ee2540253e54b2
דור שפירא <Dor.Shapira@outlook.com>
2023-02-04 00:12:58 +0200

    started working on the lexer

---
+++ Parser/lexer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DorSh on 04-Feb-23.
+//
+
+#include "lexer.h"
---
+++ Parser/lexer.h
@@ -0,0 +1,14 @@
+//
+// Created by DorSh on 04-Feb-23.
+//
+
+#ifndef STYPY_LEXER_H
+#define STYPY_LEXER_H
+
+
+class lexer {
+
+};
+
+
+#endif //STYPY_LEXER_H
